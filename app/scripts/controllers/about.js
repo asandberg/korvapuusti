1fea2018a08125e12ec3fe7332217784f631ef64
Antti Sandberg <antti.sandberg@gmail.com>
2016-05-26 18:54:18 +0900

    Angular setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name korvapuustiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the korvapuustiApp
+ */
+angular.module('korvapuustiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name korvapuustiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the korvapuustiApp
+ */
+angular.module('korvapuustiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
